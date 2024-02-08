3515c361128f2cfc6fc9ef21a6d803f0dcbee890
anadantasp <caroldantasp@gmail.com>
2024-02-08 16:31:11 -0300

    Implementando Logging nos Microservices com Log4J2

--- src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -6,6 +6,7 @@ import com.ead.authuser.enums.UserType;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-
+@Log4j2
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/auth")
@@ -27,10 +28,13 @@ public class AuthenticationController {
     @PostMapping("/signup")
     public ResponseEntity<Object> registerUser(@RequestBody @Validated(UserDto.UserView.RegistrationPost.class)
                                                    @JsonView(UserDto.UserView.RegistrationPost.class) UserDto userDto){
+        log.debug("POST registerUser userDTO received {} ", userDto.toString());
         if(userService.existsByUsername(userDto.getUsername())){
+            log.warn("Username {} is already taken", userDto.getUsername());
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: username is already taken");
         }
         if(userService.existsByEmail(userDto.getEmail())){
+            log.warn("Email {} is already taken", userDto.getEmail());
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: email is already taken");
         }
 
@@ -42,7 +46,9 @@ public class AuthenticationController {
         userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
 
         userService.save(userModel);
-
+        log.debug("POST registerUser userModel saved {} ", userModel.toString());
+        log.info("User saved successfully userId {}", userModel.getUserId());
         return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
     }
+
 }
