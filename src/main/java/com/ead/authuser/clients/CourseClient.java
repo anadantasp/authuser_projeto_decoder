8ec864b0d5542f421ddb88c1f139b3b8be00e5da
anadantasp <caroldantasp@gmail.com>
2024-08-26 12:33:10 -0300

    implementando circuit breaker resillience4j

--- src/main/java/com/ead/authuser/clients/CourseClient.java
+++ src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -3,6 +3,7 @@ package com.ead.authuser.clients;
 import com.ead.authuser.dtos.CourseDto;
 import com.ead.authuser.dtos.ResponsePageDto;
 import com.ead.authuser.services.UtilsService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +36,7 @@ public class CourseClient {
 
 
     //@Retry(name = "retryInstance", fallbackMethod = "retryfallback")
+    @CircuitBreaker(name = "circuitbreakerInstance")
     public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable){
         List<CourseDto> searchResult = null;
         ResponseEntity<ResponsePageDto<CourseDto>> result = null;
@@ -42,6 +44,7 @@ public class CourseClient {
 
         log.debug("Request URL: {}", url);
         log.info("Request URL: {}", url);
+        System.out.println("---Start Request ao Course Microservice");
         try{
             ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType = new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {};
             result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
@@ -54,6 +57,12 @@ public class CourseClient {
         log.info("Ending request /courses userId {} ", userId);
         return result.getBody();
 
+    }
+
+    public Page<CourseDto> circuitbreakerfallback(UUID userId, Pageable pageable, Throwable t) {
+        log.error("Inside circuit breaker fallback, cause - {}", t.toString());
+        List<CourseDto> searchResult = new ArrayList<>();
+        return new PageImpl<>(searchResult);
     }
 
     public Page<CourseDto> retryfallback(UUID userId, Pageable pageable, Throwable t){
